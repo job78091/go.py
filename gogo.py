4ffba66251193b259dcba91bf661b92772e1537a
job78091 <nvuhasv@outlook.com>
2024-05-10 10:11:54 +0100

    Create gogo.py

---
+++ gogo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 文件名：test.py
+
+if True:
+    print ("Answer")
+    print ("True")
+else:
+    print ("Answer")
+    # 没有严格缩进，在执行时会报错
+  print ("False")
+word = 'word'
+sentence = "这是一个句子。"
+paragraph = """这是一个段落。
+包含了多个语句"""
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 文件名：test.py
+
+# 第一个注释
+print ("Hello, Python!")  # 第二个注释
